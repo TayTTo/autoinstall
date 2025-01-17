de393ae31c6fab97797a245cf3339f75cbf19166
taytaoto <tayto@taytaoto.lan>
2025-01-17 15:08:23 +0700

    add tmuxifier

--- install.sh
+++ install.sh
@@ -40,5 +40,7 @@ if [  ! -d "$HOME/.tmux/plugins/tpm" ]; then
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-	
+if [ ! -d "$HOME/.tmuxifier" ]; then
+	git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+fi
 
