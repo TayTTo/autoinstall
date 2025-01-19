32767c4b096001adb2cbdb616b7fdc67fd2d682f
taytaoto <tayto@taytaoto.lan>
2025-01-19 20:54:12 +0700

    add autoinstall lazy git

--- install.sh
+++ install.sh
@@ -49,3 +49,12 @@ if [ ! -d "$HOME/.local/bin/fd" ]; then
 	mkdir -p "$HOME/.local/bin/fd"
 	ln -s $(which fdfind) ~/.local/bin/fd
 fi
+
+if [ ! -d "$HOME/Downloads/lazygitDown" ]; then
+	cd ~/Downloads
+	mkdir lazygitDown
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit -D -t /usr/local/bin/
+fi
